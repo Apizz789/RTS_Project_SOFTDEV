f43ba9b85886e54cbaef115275bc5aa667217c4b
APIRAK OULIS <82326608+Apizz789@users.noreply.github.com>
2021-10-22 20:37:49 +0700

    fix bug2

--- Fontend/rail-transport-system/src/components/Register/SignUp/TrainSignup.js
+++ Fontend/rail-transport-system/src/components/Register/SignUp/TrainSignup.js
@@ -6,50 +6,50 @@ function TrainSignup() {
             <h1 align="center">REGISTRATION</h1>
             
           
-            <div class="input-box">
+            <div class="input-box" align="center">
                 <span class = "details">
                 <h2>First Name</h2></span>
                 <input type = "text" placeholder = "Input your First Name" required></input>
             </div>
 
-            <div class="input-box">
+            <div class="input-box" align="center">
                 <span class = "details">
                 <h2>Last Name</h2></span>
                 <input type = "text" placeholder = "Input your Last Name" required></input>
             </div>
 
-            <div class="input-box">
+            <div class="input-box" align="center">
                 <span class = "details">
                 <h2>User</h2></span>
                 <input type = "text" placeholder = "Input your User" required></input>
             </div>
 
-            <div class="input-box">
+            <div class="input-box" align="center">
                 <span class = "details">
                 <h2>Password</h2></span>
                 <input type = "text" placeholder = "Input your Password" required></input>
             </div>
 
-            <div class="input-box">
+            <div class="input-box" align="center">
                 <span class = "details">
                 <h2>Telephone</h2></span>
                 <input type = "text" placeholder = "Input your Telephone" required></input>
             </div>
 
-            <div class="input-box">
+            <div class="input-box" align="center">
                 <span class = "details">
                 <h2>DOB</h2></span>
                 <input type = "text" placeholder = "Input your DOB" required></input>
             </div>
 
-            <div class="input-box">
+            <div class="input-box" align="center">
                 <label class="container">Male
                 <input type="checkbox" checked="checked"></input>
                 <span class="checkmark"></span>
                 </label>
             </div>
-            
-            <div class="input-box">
+
+            <div class="input-box" align="center">
                 <label class="container">Female
                 <input type="checkbox" checked="checked"></input>
                 <span class="checkmark"></span>
@@ -57,7 +57,7 @@ function TrainSignup() {
             </div>
            
 
-            <div class = "button">
+            <div class = "button" align="center">
                 <input type= "submit" value = "Register"></input>
             </div>
             
