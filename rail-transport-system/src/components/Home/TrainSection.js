eb476532b6cd89f1f503e0f2e4349b00d9bf115b
62011019 <p.pluemapirak@gmail.com>
2021-10-22 14:57:32 +0700

    .

--- rail-transport-system/src/components/Home/TrainSection.js
+++ rail-transport-system/src/components/Home/TrainSection.js
@@ -3,24 +3,32 @@ import { Link } from 'react-router-dom';
 import '../../App.css';
 import { Button } from '../Button/Button';
 import '../Home/TrainSection.css'
-function TrainSection(){
-    return(
-        <div className='train-container'>
-         <video src="/videos/video-2.mp4" autoPlay loop muted />
-         <h1>Welcome to Skytrain In Bangkok</h1>
-         <h2>กรุงเทพ ชีวิตดีๆที่ลงตัว</h2>
-         <li>
-            <Link to='/ticket'>
-                    <div className="train-btns">
-                            <Button className='btns' 
-                            buttonStyle='btn--outline'
-                            buttonSize='btn--large'>
-                                GET TICKET  <i class="fas fa-ticket-alt" />
-                            </Button>
-                    </div>
-            </Link>
-         </li>
-        </div>
+
+function TrainSection() {
+    return ( <
+        div className = 'train-container' >
+        <
+        video src = "/videos/video-2.mp4"
+        autoPlay loop muted / >
+        <
+        h1 > Welcome to Skytrain In Bangkok < /h1> <
+        h2 > กรุงเทพ ชีวิตดี ๆ ที่ ลงตั ว < /h2> <
+        li >
+        <
+        Link to = '/ticket' >
+        <
+        div className = "train-btns" >
+        <
+        Button className = 'btns'
+        buttonStyle = 'btn--outline'
+        buttonSize = 'btn--large' >
+        GET TICKET < i class = "fas fa-ticket-alt" / >
+        <
+        /Button> <
+        /div> <
+        /Link> <
+        /li> <
+        /div>
     )
 }
 
