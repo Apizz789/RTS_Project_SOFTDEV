0f1050e931c3aa6309dbeac754e6415a636dc438
62011019 <p.pluemapirak@gmail.com>
2021-10-22 14:58:57 +0700

    .

--- rail-transport-system/src/App.js
+++ rail-transport-system/src/App.js
@@ -1,22 +1,34 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router,Switch,Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/pages/Home';
 import Services from './components/pages/Services';
+import Signup from './components/pages/Signup';
 
 function App() {
-  return (
-    <>
-    <Router>
-      <Navbar />
-      <Switch>
-        <Route path='/' exact  component ={Home} />
-        <Route path='/services' exact component ={Services} />
-      </Switch>
-    </Router>
-    </>
-  );
+    return ( <
+        >
+        <
+        Router >
+        <
+        Navbar / >
+        <
+        Switch >
+        <
+        Route path = '/'
+        exact component = { Home }
+        /> <
+        Route path = '/services'
+        exact component = { Services }
+        /> <
+        Route path = '/signup'
+        exact component = { Signup }
+        /> <
+        /Switch> <
+        /Router> <
+        />
+    );
 }
 
 export default App;
