e3788a25a0f1b4564b76cfa25d1bb753faa9a2f1
62011019 <p.pluemapirak@gmail.com>
2021-10-22 14:53:21 +0700

    .

---
+++ rail-transport-system/src/components/SignUp/Signup.js
@@ -0,0 +1,9 @@
+import React, { memo } from 'react'
+
+export default memo(function signup() {
+    return ( <
+        div >
+        Your account <
+        /div>
+    )
+})
---
+++ rail-transport-system/src/components/pages/Signup.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../../App.css';
+import Signup from '../SignUp/Signup';
+// import Cards from '../Card/Cards';
+
+function Home() {
+    return ( <
+        >
+        <
+        Signup / >
+
+        <
+        />
+    )
+}
+
+export default Signup;
