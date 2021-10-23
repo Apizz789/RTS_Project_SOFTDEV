493caceac48c42f726cd34b0387629a35c43c11c
dinkxz <dink.4567@gmail.com>
2021-10-23 20:59:36 +0700

    Footer(test)

--- Fontend/rail-transport-system/src/components/Footer(Contact)/Footer.js
+++ Fontend/rail-transport-system/src/components/Footer(Contact)/Footer.js
@@ -6,57 +6,26 @@ import { Link } from 'react-router-dom';
 function Footer() {
   return (
     <div className='footer-container'>
-      <section className='footer-subscription'>
-        <p className='footer-subscription-heading'>
-          Join the Adventure newsletter to receive our best vacation deals
-        </p>
-        <p className='footer-subscription-text'>
-          You can unsubscribe at any time.
-        </p>
-        <div className='input-areas'>
-          <form>
-            <input
-              className='footer-input'
-              name='email'
-              type='email'
-              placeholder='Your Email'
-            />
-            <Button buttonStyle='btn--outline'>Subscribe</Button>
-          </form>
-        </div>
-      </section>
       <div class='footer-links'>
         <div className='footer-link-wrapper'>
           <div class='footer-link-items'>
             <h2>About Us</h2>
-            <Link to='/sign-up'>How it works</Link>
-            <Link to='/'>Testimonials</Link>
-            <Link to='/'>Careers</Link>
-            <Link to='/'>Investors</Link>
+            <Link to='/sign-up'>Signup</Link>
+            <Link to='/'>Map</Link>
+            <Link to='/'>ข้อปฏิบัติ</Link>
+            <Link to='/'>Calculate</Link>
             <Link to='/'>Terms of Service</Link>
           </div>
           <div class='footer-link-items'>
             <h2>Contact Us</h2>
             <Link to='/'>Contact</Link>
-            <Link to='/'>Support</Link>
-            <Link to='/'>Destinations</Link>
-            <Link to='/'>Sponsorships</Link>
-          </div>
-        </div>
-        <div className='footer-link-wrapper'>
-          <div class='footer-link-items'>
-            <h2>Videos</h2>
-            <Link to='/'>Submit Video</Link>
-            <Link to='/'>Ambassadors</Link>
-            <Link to='/'>Agency</Link>
-            <Link to='/'>Influencer</Link>
           </div>
           <div class='footer-link-items'>
             <h2>Social Media</h2>
-            <Link to='/'>Instagram</Link>
-            <Link to='/'>Facebook</Link>
-            <Link to='/'>Youtube</Link>
-            <Link to='/'>Twitter</Link>
+            {/* <Link to='/'>Instagram</Link> */}
+            <a href='https://www.facebook.com/piyaphatboontham/'>Facebook</a>
+            {/* <Link to='/'>Youtube</Link>
+            <Link to='/'>Twitter</Link> */}
           </div>
         </div>
       </div>
@@ -64,20 +33,20 @@ function Footer() {
         <div class='social-media-wrap'>
           <div class='footer-logo'>
             <Link to='/' className='social-logo'>
-              TRVL
-              <i class='fab fa-typo3' />
+              RTS
+              <i class="fas fa-subway"></i>
             </Link>
           </div>
-          <small class='website-rights'>TRVL © 2020</small>
+          <small class='website-rights'>RTS © 2021</small>
           <div class='social-icons'>
-            <Link
+            <a
               class='social-icon-link facebook'
-              to='/'
+              href='https://www.facebook.com/piyaphatboontham/'
               target='_blank'
               aria-label='Facebook'
             >
               <i class='fab fa-facebook-f' />
-            </Link>
+            </a>
             <Link
               class='social-icon-link instagram'
               to='/'
@@ -93,22 +62,6 @@ function Footer() {
               aria-label='Youtube'
             >
               <i class='fab fa-youtube' />
-            </Link>
-            <Link
-              class='social-icon-link twitter'
-              to='/'
-              target='_blank'
-              aria-label='Twitter'
-            >
-              <i class='fab fa-twitter' />
-            </Link>
-            <Link
-              class='social-icon-link twitter'
-              to='/'
-              target='_blank'
-              aria-label='LinkedIn'
-            >
-              <i class="fab fa-linkedin"></i>
             </Link>
           </div>
         </div>
--- Fontend/rail-transport-system/src/components/pages/Home.js
+++ Fontend/rail-transport-system/src/components/pages/Home.js
@@ -10,7 +10,7 @@ function Home(){
         <>
             <TrainSection />
             <Cards />
-            {/* <Footer /> */}
+            <Footer />
         </>
     )
 }
