b24ff13c960ad344aeb357447a53ba373ac87bab
dinkxz <dink.4567@gmail.com>
2021-10-27 21:01:48 +0700

    map update

--- Fontend/rail-transport-system/src/components/Map/Trainmap.js
+++ Fontend/rail-transport-system/src/components/Map/Trainmap.js
@@ -2,6 +2,7 @@ import React, { memo } from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import { Link } from 'react-router-dom';
 import './Trainmap.css'
 export default memo(function Trainmap() {
   return (
@@ -11,17 +12,17 @@ export default memo(function Trainmap() {
         <Col>BTS,MRT and AIRPORTLINK</Col>
       </Row>
       <Row>
-        <Col>รูปแผนที่</Col>
+        <Col><img src='images/mapfull2564.jpg' style={{width: 600,height: 'auto'}}></img></Col>
         <Col>
           <Col>
               <Col>
-                    <img src='images/BTS.jpg'></img>
+                    <img src='images/BTS.jpg' style={{width: 280,height: 'auto'}}></img>
               </Col>
               <Col>
-                    <img src='images/MRT.jpg'></img>
+                    <img src='images/MRT.jpg'style={{width: 280,height: 'auto'}}></img>
               </Col>
               <Col>
-                    <img src='images/airlink.jpg'></img>
+                    <img src='images/airlink.jpg' style={{width: 280,height: 'auto'}}></img>
               </Col>
           </Col>
           <Col>วิธีดูหมายเลขสถานี</Col>
@@ -30,37 +31,45 @@ export default memo(function Trainmap() {
               <Col>
                 <Row>
                   <Col xs="12" >
-                    <i className="fas fa-check-square"></i>
-                    ตัวอักษรภาษาอังกฤษแสดงชื่อของสายรถไฟ
+                    <Row>
+                      <Col xs="2"><i className="fas fa-check-square"></i></Col>
+                      <Col  style={{textAlign:"left"}}>ตัวอักษรภาษาอังกฤษแสดงชื่อของสายรถไฟ</Col>
+                      <div style={{ borderTop: "5px solid #7f7df9 ", marginLeft: 15 ,width: 225}}></div>
+                    </Row>
                   </Col>
-                  <Col xs="12">
-                    <i className="fas fa-check-square"></i>
-                    หมายเลขสถานีจะแสดงตามสายของรถไฟในแผนที่เส้นทางรถไฟใต้ดิน
+                  <Col xs="12" >
+                    <Row>
+                      <Col xs="2"><i className="fas fa-check-square"></i></Col>
+                      <Col  style={{textAlign:"left"}}>หมายเลขสถานีจะแสดงตามสายของรถไฟในแผนที่เส้นทางรถไฟใต้ดิน</Col>
+                      <div style={{ borderTop: "5px solid #7f7df9 ", marginLeft: 15 ,width: 225}}></div>
+                    </Row>
                   </Col>
-                  <Col xs="12">
-                    <i className="fas fa-check-square"></i>
-                    หมายเลขสถานีจะล้อมรอบด้วยวงกลมสีซึ่งแสดงถึงสีของสายรถไฟนั้น
+                  <Col xs="12" >
+                    <Row>
+                      <Col xs="2"><i className="fas fa-check-square"></i></Col>
+                      <Col  style={{textAlign:"left"}}>หมายเลขสถานีจะล้อมรอบด้วยวงกลมสีซึ่งแสดงถึงสีของสายรถไฟนั้น</Col>
+                    </Row>
                   </Col>
                 </Row>
               </Col>
-              <Col>
+              <Col xs="7">
                   <Row>
-                      <Col>BTS(สีลม)</Col>
-                      <Col>MRT(สายสีน้ำเงิน)</Col>
+                      <Col><img src='images/btsgreen.png' style={{width: 150,height: 'auto'}}></img></Col>
+                      <Col><img src='images/mrtblue.png' style={{width: 150,height: 'auto'}}></img></Col>
                   </Row>
                   <Row>
-                      <Col>BTS(สายสีเขียวอ่อน)</Col>
-                      <Col>MRT(สีม่วง)</Col>
+                      <Col><img src='images/btsgreenlight.png' style={{width: 150,height: 'auto'}}></img></Col>
+                      <Col><img src='images/mrtpurple.png' style={{width: 150,height: 'auto'}}></img></Col>
                   </Row>
                   <Row>
-                      <Col>BTS(สายสีทอง)</Col>
-                      <Col>ART</Col>
+                      <Col><img src='images/btsgold.png' style={{width: 150,height: 'auto'}}></img></Col>
                   </Row>
               </Col>
             </Row>
           </Row>
         </Col>
       </Row>
+      <Link to='/calculate'><i class="far fa-arrow-alt-circle-down"></i>คำนวณระยะทางค่าโดยสาร<i class="far fa-arrow-alt-circle-down"></i></Link>
     </Container>
   );
 });
