cac887ee22d6ee488d8fb0fbf16773c45d6fb42b
rapeepatpe <54279530+rapeepatpe@users.noreply.github.com>
2021-11-12 20:45:47 +0700

    .

--- Fontend/rail-transport-system/src/components/Register/TrainRegister.js
+++ Fontend/rail-transport-system/src/components/Register/TrainRegister.js
@@ -1,10 +1,80 @@
 import React, {useState, useReducer } from 'react'
 import './TrainRegister.css'
+import axios from 'axios'
+import validation_register from "./validation_register"
 import {Button,Form} from 'react-bootstrap';
 
 function TrainRegister() {
+    const [values, setValues] = useState({
+        fname: "",
+        lname: "",
+        username: "",
+        password: "",
+        repeat_password: "",
+        tel: "",
+        DOB: "",
+        email: "",
+        sex: "",
+    });
+    const [errors, setErrors] = useState({
+        fname: "",
+        lname: "",
+        username: "",
+        password: "",
+        repeat_password: "",
+        tel: "",
+        DOB: "",
+        email: "",
+        sex: "",
+    });
+
+
+    const initialState = {
+        change: false,
+        click: true,
+    };
+
+    const reducer = (state, action) => ({ ...state, ...action });
+    const [state, setState] = useReducer(reducer, initialState);
+
+    const handleChange = (event) => {
+        setValues({
+            ...values,
+            [event.target.name]: event.target.value,
+        });
+    }
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        // setErrors(validation_signup(values));
+
+        
+
+        if (Object.keys(errors).length === 0) {
+            
+            axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDwZnikVYpl2Rh8xMOSSIqO0HLXKaoxoOI',{
+                        "email": values.email,
+                        "password": values.password,
+                        "returnSecureToken": true
+                        })
+                         .then(response => {console.log(response)})
+                         .catch(error => {console.log(error)})
+
+            axios.post('https://us-central1-soft-dev-tutorial.cloudfunctions.net/users', {
+                "Firstname": values.fname,
+                "Lastname": values.lname,
+                "Username": values.username,
+                "Password": values.password,
+                "Tel": values.tel,
+                "DOB": values.DOB,
+                "Email": values.email,
+                "Sex": values.sex,
+            })
+                .then(response => { console.log(response) })
+                .catch(error => { console.log(error) })
+        }
+    }
     return (
-        <div className="Body_Regis">
+        <div className="Body_Regis" style={{marginTop :"50px"}}>
                 <title>Slide Navbar</title>
             <link rel="stylesheet" type="text/css" href="slide navbar style.css" />
             <link href="https://fonts.googleapis.com/css2?family=Jost:wght@500&display=swap" rel="stylesheet" />
@@ -34,12 +104,62 @@ function TrainRegister() {
                     />
                 </div>
                 <div className="Info-User" id="right">
-                    <input type="text" name="txt" placeholder="User name" required />
-                    <input type="email" name="email" placeholder="Email" required />
-                    <input type="password" name="pswd" placeholder="Password" required />
-                    <input type="re-password" name="repswd" placeholder="RE- Password" required />
-                    <input type="Telephone" name="tel" placeholder="Tel" required />
-                    <input classname="inputbox" type="date" data-date-inline-picker="true" name="DOB" min="1900-01-01" max="2009-12-31" placeholder="dd-mm-yyyy" required defaultValue="{values.DOB}" onchange="{handleChange}" />
+
+                    <input 
+                        type="text" 
+                        name="txt" 
+                        placeholder="User name" 
+                        required 
+                        value={values.username}
+                        onChange={handleChange}
+                    />
+
+                    <input 
+                        type="email" 
+                        name="email" 
+                        placeholder="Email" 
+                        required
+                        value={values.email}
+                        onChange={handleChange}
+                    />
+
+                    <input 
+                        type="password" 
+                        name="pswd" 
+                        placeholder="Password" 
+                        required
+                        value={values.password}
+                        onChange={handleChange}
+                    />
+
+                    <input 
+                        type="re-password" 
+                        name="repswd" 
+                        placeholder="RE- Password" 
+                        required
+                        value={values.repeat_password}
+                        onChange={handleChange}
+                    />
+
+                    <input 
+                        type="Telephone" 
+                        name="tel" 
+                        placeholder="Tel" 
+                        required
+                        value={values.tel}
+                        onChange={handleChange}
+                    />
+
+                    <input 
+                        classname="inputbox" 
+                        type="date" 
+                        data-date-inline-picker="true" 
+                        name="DOB" min="1900-01-01" 
+                        max="2009-12-31" 
+                        placeholder="dd-mm-yyyy" 
+                        required 
+                        value={values.DOB} 
+                        onchange={handleChange} />
                 </div>
                 <div id="right">
                     <Button variant="outline-danger">Cancel</Button>{' '}
---
+++ Fontend/rail-transport-system/src/components/Register/validation_register.js
@@ -0,0 +1,98 @@
+
+import axios from 'axios'
+
+let user_list = []
+
+async function makeGetRequest() {
+
+    const fetchedResult = [];
+    user_list = []
+    const res = await axios.get('https://us-central1-soft-dev-tutorial.cloudfunctions.net/users');
+    for (let key in res.data) {
+    fetchedResult.unshift(
+        {
+            // ...res.data[key],
+            username: res.data[key].Username,
+            password: res.data[key].Password,
+            id: key
+        }
+    )
+    user_list.push(res.data[key].Username)
+    }
+    // let reversed = user_list.reverse();
+}
+
+makeGetRequest()
+
+const validation_register = (values) => {
+    let errors={};
+    
+    console.log(user_list)
+    values.fname = values.fname.replace(/\s+/g, '');
+    if(!values.fname){
+        errors.fname = "First Name is required"
+    }
+    else if(!values.fname.match(/^([A-Z]|[a-z]|[ๅภถุึคตจขชไำพะัีรนยบลฃฟหกดเ้่าสวงผปแอิืทมใฝูฎฑธํ๊ณญฐฅฤฆฏโฌ็๋ษศซฉฮฺ์ฒฬฦ]|[ ])+$/i)){
+        errors.fname = 'First Name must contains only A-Z , a-z'
+    }
+
+    values.lname = values.lname.replace(/\s+/g, '');
+    if(!values.lname){
+        errors.lname = "Last Name is required"
+    }
+    else if(!values.lname.match(/^([A-Z]|[a-z]|[ๅภถุึคตจขชไำพะัีรนยบลฃฟหกดเ้่าสวงผปแอิืทมใฝูฎฑธํ๊ณญฐฅฤฆฏโฌ็๋ษศซฉฮฺ์ฒฬฦ]|[ ])+$/i)){
+        errors.lname = 'Last Name must contains only A-Z , a-z'
+    }
+
+    if(!values.username){
+        errors.username = "Username is required"
+    }
+    else if(values.username.length < 8 || values.username.length > 20){
+        errors.username = "Username length must be between 8-20 characters"
+    }
+    else if (Object.values(user_list).includes(values.username)){
+        errors.username = "This Username is already taken."
+    }
+
+
+
+    if(!values.password){
+        errors.password = "Password is required"
+    }
+    else if(values.password.length < 8 || values.password.length > 32){
+        errors.password = "Password length must be between 8-32 characters"
+    }
+    // else if(!values.password.match(/^(?=.[a-z])(?=.[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/))
+    // {
+    //     errors.password = "Password must contains A-z, a-z and 0-9 mix together"
+    // } 
+
+
+    if(values.password != values.repeat_password){
+        errors.repeat_password = "Password Not Match!!"
+    }
+
+
+    if(!values.tel){
+        errors.tel = "Tel. is required"
+    }
+    else if(values.tel.length < 10 || values.tel[0] != '0'){
+        errors.tel = "Tel Invalid"
+    }
+
+
+    if(!values.DOB){
+        errors.DOB = "Date of Birth is required"
+    }
+
+
+    if(!values.email){
+        errors.email = "Email is required"
+    }
+    else if(!/\S+@\S+\.\S+/.test(values.email)){
+        errors.email = "Email is invalid"
+    }
+
+    return errors;
+}
+export default validation_register;
