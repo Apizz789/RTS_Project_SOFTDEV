2f01fdeb090e133899f4b3d588ac188e767e8edf
62011019 <p.pluemapirak@gmail.com>
2021-11-14 07:48:12 +0700

    .

--- Fontend/rail-transport-system/src/components/CalculationResult/TrainCalculationResult.js
+++ Fontend/rail-transport-system/src/components/CalculationResult/TrainCalculationResult.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import Spinner from 'react-bootstrap/Spinner'
 function refreshPage(){
 	window.scrollTo(0,0);
   }
@@ -17,12 +17,16 @@ function TrainCalculationResult() {
             <br></br>
             <br></br>
 
-            <div className="result">
+            <div className="result" style={{textAlign:"center"}}>
             <h2 align="center">สถานีต้นทาง : ______________________________</h2>
             <h3 align="center">สถานีปลายทาง : ______________________________</h3>
             <h4 align="center">เส้นทางที่คำนวณได้ : ______________________________</h4>
             <h4 align="center">เวลาที่คำนวณได้ : ______________________________</h4>
             <h5 align="center">ราคาที่คำนวณได้ : ______________________________</h5>
+            <br></br>
+            <Spinner animation="grow" variant="info" />
+            <Spinner animation="grow" variant="info" />
+            <Spinner animation="grow" variant="info" />
             </div>
             
             <br></br>
--- Fontend/rail-transport-system/src/components/Slidebar/TrainSlidebar.js
+++ Fontend/rail-transport-system/src/components/Slidebar/TrainSlidebar.js
@@ -1,24 +1,28 @@
 import React from 'react'
 import Carousel from 'react-bootstrap/Carousel'
+import './TrainSlidebar.css';
 
 
 function refreshPage(){
 	window.scrollTo(0,0);
   }
   
-
+  
 function TrainSlidebar() {
+   
     return (
-        <div style={{marginTop:"55px"}}>
+        <div style={{marginTop:"155px"}}>
             <Carousel>
   <Carousel.Item>
     <img
       className="d-block w-100"
       src="../images/BN2.png?text=First slide&bg=373940"
       alt="First slide"
+
     />
     <Carousel.Caption>
-      <h3 >First slide label</h3>
+    <img src="../images/BTS-Logo.png"/> 
+      <h3 >Bangkok Mass Transit System</h3>
       <p >Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
     </Carousel.Caption>
   </Carousel.Item>
@@ -30,7 +34,8 @@ function TrainSlidebar() {
     />
 
     <Carousel.Caption>
-      <h3 >Second slide label</h3>
+    <img src="../images/MRT-Logo.png"/>  
+      <h3 >Mass Rapid Transit</h3>
       <p >Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
     </Carousel.Caption>
   </Carousel.Item>
@@ -39,11 +44,12 @@ function TrainSlidebar() {
       className="d-block w-100"
       src="../images/BN1.png?text=Third slide&bg=20232a"
       alt="Third slide"
-      width="500" height="600"
+
     />
 
     <Carousel.Caption>
-      <h3 >Third slide label</h3>
+     <img src="../images/ARL-Logo.png"/> 
+      <h3 >S.R.T. Electrified Train Co., Ltd.</h3>
       <p >Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
     </Carousel.Caption>
   </Carousel.Item>
