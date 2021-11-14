27bc6a42924b20b49e171d39fecddedf8613e7a4
62011019 <p.pluemapirak@gmail.com>
2021-11-15 03:40:00 +0700

    pure

--- Fontend/rail-transport-system/src/components/Flippage/Flip.js
+++ Fontend/rail-transport-system/src/components/Flippage/Flip.js
@@ -2,51 +2,34 @@ import React,{useState} from 'react'
 import ReactCardFlip from 'react-card-flip';
 
 function Flip() {
-    const [isFlipped, setIsFlipped] = useState(false);
-    const handleClick = () => {setIsFlipped(!isFlipped);};
+    
     return (
         <div style={{marginTop:"55px"}}>
-            <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
-                <div
-                    style={{
-                    backgroundColor: "green",
-                    height: 300,
-                    width:1000,
-                    color: "#fff",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center"
-                    }}
-                >
-
-
-                    sdsdfsdfsdfsdf<br></br>
-                    
-
-
-                    <button onClick={handleClick}>Click to flip</button>
-                </div>
-
-                <div
-                    style={{
-                    backgroundColor: "yellow",
-                    height: 300,
-                    width:1000,
-                    color: "#fff",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center"
-                    }}
-                >
-
-
-                    Create video tutorials on YouTube
-
-
-
-                    <button onClick={handleClick}>Click to flip</button>
-                </div>
-            </ReactCardFlip>
+            <br ></br>
+            <br ></br>
+            <br ></br>
+            <br ></br>
+            <br ></br>
+            <br ></br>
+            <br ></br>
+            <br ></br>
+            <br ></br>
+            <br ></br>
+            <br ></br>
+            <br ></br>
+            <br ></br>
+            <br ></br>
+            <br ></br>
+            <br ></br>
+            <br ></br>
+            <br ></br>
+            <br ></br>
+            <br ></br>
+            <br ></br>
+            
+            
+               
+            
         </div>
     )
 }
