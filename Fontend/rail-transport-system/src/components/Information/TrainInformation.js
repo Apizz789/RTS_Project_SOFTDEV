482e29eac5413388fe84e881575d01fbdb24a433
62011019 <p.pluemapirak@gmail.com>
2021-11-16 03:58:18 +0700

    Infomation 70%

--- Fontend/rail-transport-system/src/components/Information/TrainInformation.js
+++ Fontend/rail-transport-system/src/components/Information/TrainInformation.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {InputGroup,FormControl,Col,Row,Button,Container,Form,Table} from 'react-bootstrap'
+import {InputGroup,FormControl,Col,Row,Button,Container,Form,Table,Accordion} from 'react-bootstrap'
 import Spinner from 'react-bootstrap/Spinner'
 import "./TrainInformation.css";
 
@@ -12,44 +12,96 @@ function TrainInformation() {
             <Container>
             
                 <Row style={{textAlign:"center"}}>
-                    <Col style={{borderRadius:"20px",width:"600px",height:"500px",margin:"10px",backgroundColor:"#fed46e"}}>
+                    <Col style={{borderRadius:"20px",width:"600px",height:"700px",margin:"10px",backgroundColor:"#fed46e", textAlign:"left"}}>
                     <br></br><h1><i class="fas fa-newspaper"></i> RTS News</h1><br></br>
-                    <marquee  behavior="alternate" scrollamount="2" style={{textAlign:"center", border:"2"}}>
-                    1.แจ้งเตือน พบการโจมตีช่องโหว่ใน Apache  <br></br>
-                    2.แจ้งเตือน พบการโจมตีช่องโหว่ใน Apache  <br></br>
-                    3.แจ้งเตือน พบการโจมตีช่องโหว่ใน Apache  <br></br>
-                    4.แจ้งเตือน พบการโจมตีช่องโหว่ใน Apache  <br></br>
-                    5.แจ้งเตือน พบการโจมตีช่องโหว่ใน Apache  <br></br>
-                    6.แจ้งเตือน พบการโจมตีช่องโหว่ใน Apache  <br></br>
-                    7.แจ้งเตือน พบการโจมตีช่องโหว่ใน Apache  <br></br>
-                    8.แจ้งเตือน พบการโจมตีช่องโหว่ใน Apache  <br></br>
+                    {/* <marquee  behavior="alternate" scrollamount="2" style={{textAlign:"center", border:"2"}}> */}
+                    [BTS]  - ประกาศ !!! ปิดสถานี สายสีเขียวอ่อน E04 อโศก Asok : Update 15/11/2021  <br></br>
+                    [MRT] - ประกาศ !!! ปิดสถานี สายสีน้ำเงิน BL21 เพชรบุรี Phetchaburi : Update 15/11/2021  <br></br>
+                    [ARL] - ประกาศ !!! ปิดสถานี สายเชื่อมท่าอากาศยาน A06 มักกะสัน Makkasan : Update 15/11/2021  <br></br>
+                    [BTS] - ประกาศ !!! แจ้งจับ นายบีทีเอส ข้อหาแสดงละครบนรางรถไฟ  : Update 14/11/2021  <br></br>
+                    [MRT] - ทั่วไป !!! ชี่นชม นายเออาร์แอล หลังคว้ารางวัลนักเดินทางดีเด่น  : Update 14/11/2021  <br></br>
+                    [ARL] - ทั่วไป !!! นายเอ็มอาร์ที ไปกองฉลากไม่ทัน เหตุจากตกขบวนรถ  : Update 12/11/2021  <br></br>
+                    . <br></br>
+                    .  <br></br>
 
                     
                     
-                   </marquee>
+                   {/* </marquee> */}
                     </Col>
-                    <Col style={{borderRadius:"20px",width:"600px",height:"500px",margin:"10px",backgroundColor:"#fad4ae"}}>
+                    <Col style={{borderRadius:"20px",width:"600px",height:"700px",margin:"10px",backgroundColor:"#fad4ae"}}>
                     <br></br><h1><i class="fas fa-info"></i> Infomation</h1><br></br>
 
-                    <marquee  behavior="scroll" direction="up" scrollamount="2" style={{textAlign:"center", border:"2"}}>
-                    สวัสดีการเคหะ สวัสดีการเคหะ สวัสดีการเคหะ สวัสดีการเคหะ<br></br>
-                    สวัสดีการเคหะ สวัสดีการเคหะ สวัสดีการเคหะ สวัสดีการเคหะ<br></br>
-                    สวัสดีการเคหะ สวัสดีการเคหะ สวัสดีการเคหะ สวัสดีการเคหะ<br></br>
-                    สวัสดีการเคหะ สวัสดีการเคหะ สวัสดีการเคหะ สวัสดีการเคหะ<br></br>
-                    สวัสดีการเคหะ สวัสดีการเคหะ สวัสดีการเคหะ สวัสดีการเคหะ<br></br>
-                    สวัสดีการเคหะ สวัสดีการเคหะ สวัสดีการเคหะ สวัสดีการเคหะ
+                    <marquee  behavior="scroll" direction="up" scrollamount="2" style={{textAlign:"left", border:"2"}}>
+
+                    <i class="fas fa-comment-dots"></i> RTS ขอต้อนรับทุกท่านเข้าสู่ โลกแห่งการไร้พรหมแดน <br></br>
+                    ที่จะพาทุกคนสะดวกและง่ายต่อการเดินทาง <br></br>
+                    หากมีข้อสงสัย ติดต่อเรราได้ที่ <b>Call center 02-999-9999</b> <br></br>
+                    และช่องทางออนไลน์ <b>info@railtransportsystems.com</b><br></br><br></br>
+
+                    <i class="fas fa-comment-dots"></i> เธอห่วงใครก็ไม่เท่าเราห่วงเธอ แต่ RTS ห่วงคุณเสมอ<br></br>
+                    เพื่อความปลอดภัยของผู้ใช้งาน กรุณาเก็บรหัสผ่านไว้ อย่าให้ใครเป็นการส่วนตัว ไม่บอกต่อคนอื่น ไม่ว่าจะเจตนาอย่างไร<br></br>
+                     <br></br>
+                    ด้วยความปรารถนาจาก RTS<br></br><br></br>
+
+                    <i class="fas fa-comment-dots"></i> หน้าร้อนมันหนาว หน้าเรามันฟิน RTS พาคุณไปดื่มด่ำกับ<br></br>
+                    โปรโมชั่นที่ร้อนแรงในวินาทีนี้ กับโปรโมชั่น หน้าหนาวสุดฟิน มาฟินกับ RTS <br></br><br></br> 
+                    ฟินกับส่วนลดการเดินทาง <b>RTS ลดให้คุณทันที 10%</b><br></br> 
+                    เมื่อซื้อตั๋วกับทางเรา <b>วันนี้ถึง 31 ธันวาคม ศกนี้</b><br></br><br></br>
+
+                    <center><img src="images/RTS-LOGO.png" width = '100' height='100'style={{textAlign:"center"}} /><br></br> <big><big><big><b>RTS</b></big></big></big></center> 
+
+
                    </marquee>
                    <br></br>
                     </Col>
-                    <Col style={{borderRadius:"20px",width:"600px",height:"500px",margin:"10px",backgroundColor:"#ff7b7b",}}>
+                    <Col style={{borderRadius:"20px",width:"600px",height:"700px",margin:"10px",backgroundColor:"#ff7b7b",}}>
                     <br></br><h1><i class="fas fa-comments"></i> RTS Q&A</h1><br></br>
-                    
-                    <h7> รถไฟฟ้าบีทีเอส เปิดให้บริการ เปิด - ปิด เวลาใด </h7>
-                    <h7> รถไฟฟ้าบีทีเอสมีเส้นทางการให้บริการกี่สาย </h7>
-                    <h7> เมื่อทำสิ่งของตกลงในรางรถไฟฟ้า ผู้โดยสารควรทำอย่างไร </h7>
-                    <h7> สามารถนำสัตว์เลี้ยงเข้ารถไฟฟ้าได้ไหม </h7>
-                    <h7> รถไฟบีทีเอสมีลิฟต์ให้บริการที่สถานีใดบ้าง </h7>
-                    <h7> ศูนย์ข้อมูลนักท่องเที่ยวให้บริการเกี่ยวกับอะไรบ้าง </h7>
+
+                    <Accordion defaultActiveKey="0">
+                        <Accordion.Item eventKey="0">
+                            <Accordion.Header>ระบบ RTS คืออะไร</Accordion.Header>
+                            <Accordion.Body>
+                            55555555555555555555
+                            </Accordion.Body>
+                        </Accordion.Item>
+
+                        <Accordion.Item eventKey="1">
+                            <Accordion.Header>รถไฟฟ้าในระบบ RTS มีเส้นทางการให้บริการกี่สาย</Accordion.Header>
+                            <Accordion.Body>
+                            555555555555555555555
+                            </Accordion.Body>
+                        </Accordion.Item>
+
+                        <Accordion.Item eventKey="2">
+                            <Accordion.Header>ฉันจะทราบได้อย่างไร ว่าฉันจะเดินทางไปยังจุดหมายได้ถูกต้อง</Accordion.Header>
+                            <Accordion.Body>
+                            555555555555555555555
+                            </Accordion.Body>
+                        </Accordion.Item>
+
+                        <Accordion.Item eventKey="3">
+                            <Accordion.Header>ระบบสมาชิก RTS คืออะไร ทำอะไรได้บ้าง </Accordion.Header>
+                            <Accordion.Body>
+                            555555555555555555555
+                            </Accordion.Body>
+                        </Accordion.Item>
+
+                        <Accordion.Item eventKey="4">
+                            <Accordion.Header>ฉันจะซื้อตั๋วผ่านระบบ RTS ได้อย่างไร</Accordion.Header>
+                            <Accordion.Body>
+                            555555555555555555555
+                            </Accordion.Body>
+                        </Accordion.Item>
+
+                        <Accordion.Item eventKey="5">
+                            <Accordion.Header>หากมีข้อสงสัยเกี่ยวกับระบบ ฉันจะติดต่อใคร</Accordion.Header>
+                            <Accordion.Body>
+                            555555555555555555555
+                            </Accordion.Body>
+                        </Accordion.Item>
+
+                    </Accordion>
+                
 
                     </Col>
                 </Row>
@@ -63,23 +115,35 @@ function TrainInformation() {
                     </Col>
 
                     <Col style={{borderRadius:"20px",margin:"10px",backgroundColor:"#95e3bb",}}>
-                    <br></br><h1><i class="fas fa-subway"></i> ALL TRAIN</h1><br></br>
-                    <div style={{textAlign:"center"}}>
-                    <Spinner animation="grow" variant="dark" size="sm" />___________________________________<br></br>
-                    <Spinner animation="grow" variant="dark" size="sm" />___________________________________<br></br>
-                    <Spinner animation="grow" variant="dark" size="sm" />___________________________________<br></br>
-                    <Spinner animation="grow" variant="dark" size="sm" />___________________________________<br></br>
-                    <Spinner animation="grow" variant="dark" size="sm" />___________________________________<br></br>
-                    <Spinner animation="grow" variant="dark" size="sm" />___________________________________<br></br>
-                    <Spinner animation="grow" variant="dark" size="sm" />___________________________________<br></br>
-                    <Spinner animation="grow" variant="dark" size="sm" />___________________________________<br></br>
-                    <Spinner animation="grow" variant="dark" size="sm" />___________________________________<br></br>
-                    <Spinner animation="grow" variant="dark" size="sm" />___________________________________<br></br>
-                    <Spinner animation="grow" variant="dark" size="sm" />___________________________________<br></br>
-                    <Spinner animation="grow" variant="dark" size="sm" />___________________________________
-                   
-                    </div>
-                    
+                        <br></br><h1><i class="fas fa-subway"></i> ALL TRAIN</h1><br></br>
+                        
+                        <Col>
+                        sdfsdfsdf
+                        </Col>
+                        
+            
+                        <Col>
+                            <div style={{textAlign:"left"}}>
+                            <Spinner animation="grow" variant="dark" size="sm" /> [BTS] - Bangkok Mass Transit System <i class="fas fa-caret-right"></i> สายสีเขียวอ่อน Light Green line<br></br>
+                            <Spinner animation="grow" variant="dark" size="sm" /> [BTS] - Bangkok Mass Transit System <i class="fas fa-caret-right"></i> สายสีเขียวเข้ม Dark Green line<br></br>
+                            <Spinner animation="grow" variant="dark" size="sm" /> [BTS] - Bangkok Mass Transit System <i class="fas fa-caret-right"></i> สายสีทอง Golden Line<br></br>
+                            <Spinner animation="grow" variant="dark" size="sm" /> [BTS] - Bangkok Mass Transit System <i class="fas fa-caret-right"></i> สายสีเหลือง Yellow Line<br></br>
+                            <Spinner animation="grow" variant="dark" size="sm" /> [BTS] - Bangkok Mass Transit System <i class="fas fa-caret-right"></i> สายสีชมพู Pink Line<br></br>
+                            <Spinner animation="grow" variant="dark" size="sm" /> [MRT] - Mass Rapid Transit <i class="fas fa-caret-right"></i> สายสีน้ำเงิน Blue Line<br></br>
+                            <Spinner animation="grow" variant="dark" size="sm" /> [MRT] - Airport Rail Link <i class="fas fa-caret-right"></i> สายสีม่วง Purple Line<br></br>
+                            <Spinner animation="grow" variant="dark" size="sm" /> [MRT] - Mass Rapid Transit <i class="fas fa-caret-right"></i> สายสีส้ม Orange Line<br></br>
+                            <Spinner animation="grow" variant="dark" size="sm" /> [MRT] - Mass Rapid Transit <i class="fas fa-caret-right"></i> สายสีแดง Red Line<br></br>
+                            <Spinner animation="grow" variant="dark" size="sm" /> [ARL] - Airport Rail Link <i class="fas fa-caret-right"></i> แอร์พอร์ตลิงก์ Airport Line<br></br>
+                            </div>
+                        </Col>
+
+                        <Col>
+                        
+                          sdfsdfsdf
+                        </Col>
+
+                     
+                        
                     </Col>
 
                 </Row>
