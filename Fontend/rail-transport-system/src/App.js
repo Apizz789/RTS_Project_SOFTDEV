03b11af722ef6faef61e5d3d9f1ade06c97f8930
Rinraphat <iizaimmy@gmail.com>
2021-11-15 19:38:02 +0700

    Update app (Ticket)

--- Fontend/rail-transport-system/src/App.js
+++ Fontend/rail-transport-system/src/App.js
@@ -23,12 +23,16 @@ import SelectStationDest from './components/pages/SelectStationDest';
 import Test from './components/pages/Test';
 import Flippage from './components/pages/Flippage';
 import Payment from './components/pages/Payment';
+import SelectSourceTic from './components/pages/SelectSourceTic';
+import SourceTicProvider from './components/Buyticket/UseContextSourceTic'
+import Confirm from './components/Confirm/Confirm';
 
 function App() {
 
   return (
     <SourceProvider>
     <DestProvider>
+    <SourceTicProvider>
     <Router>
       <Navbar/>
       <br/>
@@ -48,15 +52,19 @@ function App() {
         <Route path='/SelectStation' exact component ={SelectStation}/>
         <Route path='/select-station-dest' exact component ={SelectStationDest}/>
         <Route path='/ticket' exact component ={Ticket}/>
+        <Route path='/select-sorce-ticket' exact component ={SelectSourceTic}/>
         <Route path='/show-ticket' exact component ={ShowTicket}/>
         <Route path='/information' exact component ={Information} />
         <Route path='/calculation-result' exact component ={CalculationResult}/>
         <Route path='/flip-page' exact component ={Flippage}/>
+        <Route path='/Confirm' exact component ={Confirm}/>
+        <Route path="/Confirm/:confirmcode" component={Confirm} />
         <Route path='/payment' exact component ={Payment}/>
       </Switch>
       <img src='images/trainloop.gif' style={{width: "100%",height: '80px'}} draggable="false" dragstart="false" class="unselectable"></img>
       <Footer />
     </Router>
+    </SourceTicProvider>
     </DestProvider>
     </SourceProvider>
   );
