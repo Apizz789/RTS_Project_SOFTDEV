490e1ca291155dd57c6262ca944fe6f722f55c36
Brackhak <chatdanai2543@hotmail.com>
2021-11-17 02:51:48 +0700

    Valid Register

--- Fontend/rail-transport-system/src/components/Register/validation_register.js
+++ Fontend/rail-transport-system/src/components/Register/validation_register.js
@@ -54,7 +54,7 @@ const validation_register = (values) => {
     else if (Object.values(user_list).includes(values.username)){
         errors.username = <small><small><h6>"This Username is already taken."</h6></small></small>
     }
-    if(!values.username.match(/^([A-Z]|[a-z]|[0-9])/i)){
+    if(!values.username.match(/^([A-Z]|[a-z]|[0-9])+$/i)){
         errors.username = <small><small><h6>"Last Name must contains only A-Z , a-z"</h6></small></small>
     }
 
