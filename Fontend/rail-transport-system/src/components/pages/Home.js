bfffd2141588e77c01b5bdd1e6ad1026ead9648b
dinkxz <dink.4567@gmail.com>
2021-11-16 16:10:07 +0700

    Update Home.js

--- Fontend/rail-transport-system/src/components/pages/Home.js
+++ Fontend/rail-transport-system/src/components/pages/Home.js
@@ -3,13 +3,13 @@ import '../../App.css';
 import Cards from '../Card/Cards';
 import TrainSection from '../Home/TrainSection';
 import TrainSlidebar from '../Slidebar/TrainSlidebar';
-
+import TrainInformation from '../Information/TrainInformation'
 
 function Home(){
     return(
         <>
             <TrainSlidebar/>
-            <Cards />
+            <TrainInformation/>
         </>
     )
 }
