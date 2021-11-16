f4eaf372bd115d89d054e6a7c31ad7def36a7e05
dinkxz <dink.4567@gmail.com>
2021-11-16 20:39:06 +0700

    Show ticket

--- Fontend/rail-transport-system/src/components/ShowTicket/TrainShowTicket.js
+++ Fontend/rail-transport-system/src/components/ShowTicket/TrainShowTicket.js
@@ -1,9 +1,42 @@
 import React from 'react'
-
+import CardItemTicket from './CardItemTicket'
+import '../Card/Cards.css'
 function TrainShowTicket() {
     return (
-        <div style={{marginTop:"10px",width:"100%"}}>
-            ShowTicket
+        
+        <div className='cards__container'>
+            <h1>Show Ticket</h1>
+            <div className='cards__wrapper'>
+            
+                <ul className='cards__items'>
+                <CardItemTicket
+                src='images/Card/1.png'
+                text='BTS MRT AIRPORTLINK'
+                label='Rail Transport System'
+                path='/schedule'
+                />
+                <CardItemTicket
+                src='images/mapfull2564.jpg'
+                text='แผนที่รถไฟฟ้า'
+                label='Map'
+                path='/map'
+                />
+            </ul>
+            <ul className='cards__items'>
+                <CardItemTicket
+                src='images/Card/1.png'
+                text='BTS MRT AIRPORTLINK'
+                label='Rail Transport System'
+                path='/schedule'
+                />
+                <CardItemTicket
+                src='images/mapfull2564.jpg'
+                text='แผนที่รถไฟฟ้า'
+                label='Map'
+                path='/map'
+                />
+            </ul>
+            </div>
         </div>
     )
 }
