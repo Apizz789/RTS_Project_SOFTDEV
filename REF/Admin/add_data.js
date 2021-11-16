a7174700d1cfd265952a7613b52fa01c17e40de0
brackhak <54245406+Brackhak@users.noreply.github.com>
2021-11-16 19:55:29 +0700

    Add files via upload

--- REF/Admin/add_data.js
+++ REF/Admin/add_data.js
@@ -4,16 +4,16 @@ form.addEventListener('submit',(e)=>{
     e.preventDefault();
     //console.log(form.C.value);
     //console.log(form.line.value);
-    db.collection('Station').add({
-        ID:form.id.value,
-        Company:form.C.value,
-        Line:form.line.value,
-        Thai_Name:form.thaistation.value,
-        Eng_Name:form.engname.value,
-        Tran_ID:form.tran.value,
+    db.collection('Ticket').add({
+        Date_buy:"25/10/2021",
+        Date_exp:"25/12/2021",
+        S_Dest:form.line.value,
+        S_Source:form.thaistation.value,
+        ticket_id:form.engname.value,
+        user_customer:form.tran.value,
     });
     alert("เพิ่มข้อมูลแล้ว");
-    console.log(form.id.value);
+    console.log(form.ticket_id.value);
     // window.setTimeout(turn_to_main,2000);
     // function turn_to_main() {
     //      window.location.assign('test_index.html');
--- REF/Admin/app_user.js
+++ REF/Admin/app_user.js
@@ -1,24 +1,15 @@
-//const { link } = require("fs");
-let linee = 'Green'
 const db=firebase.firestore();
 const table=document.querySelector('#Tb_Show_List')
-const form=document.querySelector('#form_line');
-    // linee = form.Line_Color.value
-    console.log(linee)
-    db.collection('Station').orderBy('ID','asc').get().then((snapshot)=>{
-        //console.log(snapshot.docs); //ดึงส่วน Document มาแสดงใน console
-        snapshot.forEach(doc => {
-            // console.log(doc.data());
-            showData(doc);    
-        });
+db.collection('users').get().then((snapshot)=>{
+    //console.log(snapshot.docs); //ดึงส่วน Document มาแสดงใน console
+    snapshot.forEach(doc => {
+        // console.log(doc.data());
+        showData(doc);    
     });
-    
-
-
-
+}); 
 
 function showData(doc){
-    //Company = doc.data().cost;
+    
     //document.getElementById("demo").innerHTML = Company;
     var row=table.insertRow(-1);
     var cell1=row.insertCell(0);
@@ -27,14 +18,28 @@ function showData(doc){
     var cell4=row.insertCell(3);
     var cell5=row.insertCell(4);
     var cell6=row.insertCell(5);
-    var cell7=row.insertCell(6);    
+    var cell7=row.insertCell(6);
     var cell8=row.insertCell(7);
-    cell1.innerHTML=doc.data().Company;
-    cell2.innerHTML=doc.data().Line;
-    cell3.innerHTML=doc.data().ID;
-    cell4.innerHTML=doc.data().Thai_Name;   
-    cell5.innerHTML=doc.data().Eng_Name;
-    cell6.innerHTML=doc.data().Tran_ID;
+    var cell9=row.insertCell(8);
+    var cell10=row.insertCell(9);
+    var cell11=row.insertCell(10);
+    var cell12=row.insertCell(11);
+    cell1.innerHTML=doc.data().Username;
+    cell2.innerHTML=doc.data().Password ;
+    cell3.innerHTML=doc.data().Firstname;
+    cell4.innerHTML=doc.data().Lastname;
+    cell5.innerHTML=doc.data().Sex;
+    cell6.innerHTML=doc.data().Tel;
+    cell7.innerHTML=doc.data().DOB;
+    cell8.innerHTML=doc.data().Email;
+    cell9.innerHTML=doc.data().confirmationCode;
+    if (doc.data().status != "Active") {
+        cell10.innerHTML="Pending";
+    }
+    else {
+        cell10.innerHTML=doc.data().status;
+    }
+    
     let btn=document.createElement('button');
     let edit=document.createElement('button');
     btn.textContent='ลบข้อมูล';
@@ -43,23 +48,18 @@ function showData(doc){
     edit.textContent='แก้ไขข้อมูล';
     edit.setAttribute('class','btn.btn-green');
     edit.setAttribute('data-id',doc.id);
-    cell7.appendChild(btn);
-    cell8.appendChild(edit);
+    cell11.appendChild(btn);
+    cell12.appendChild(edit);
 
     btn.addEventListener('click',(e)=>{
         console.log(e.target.getAttribute('data-id'));
         let id=e.target.getAttribute('data-id');
-        db.collection('Station').doc(id).delete();
-
+        db.collection('users').doc(id).delete();
         alert('ลบข้อมูลแล้ว');
     });
 
     edit.addEventListener('click',(e)=> {
         let id=e.target.getAttribute('data-id');
-        console.log(id);   
-        
-        
+        console.log(id);    
     })
-
-    
 }
