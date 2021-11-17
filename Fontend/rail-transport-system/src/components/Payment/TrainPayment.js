2e9434580ba8e342fac9fe5e7d63d7248beebc58
APIRAK OULIS <82326608+Apizz789@users.noreply.github.com>
2021-11-17 09:57:17 +0700

    .

--- Fontend/rail-transport-system/src/components/Payment/TrainPayment.js
+++ Fontend/rail-transport-system/src/components/Payment/TrainPayment.js
@@ -250,7 +250,7 @@ function TrainPayment() {
                     วันที่ซื้อ : {Date}
                   </h4>
                   <h4 align="center">
-                    วันที่หมดอายุ : {Date+15}
+                    วันที่หมดอายุ : {15 วัน}
                   </h4>
                 </Col>
                 <Link to="/ticket">
